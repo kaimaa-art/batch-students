2285ea61e93b56ac459b64ed5b2f25dcf5055406
kaimaa-art <kaimaa16360@gmail.com>
2025-08-01 12:18:02 +0530

    secrate spelling changes to secret

--- router/user.js
+++ router/user.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRATE
+    api_secret: process.env.API_SECRET
 })
 
 router.post('/signup', async (req, res) => {
